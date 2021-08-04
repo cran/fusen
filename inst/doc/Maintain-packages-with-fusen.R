618d0ca0dc47e916427e1e8c3cdc6d22de1bf021
Sebastien Rochette <sebastien@thinkr.fr>
2021-08-04 12:40:02 +0000

    version 0.2.4

--- inst/doc/Maintain-packages-with-fusen.R
+++ inst/doc/Maintain-packages-with-fusen.R
@@ -7,3 +7,28 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(fusen)
 
+## ---- eval=FALSE--------------------------------------------------------------
+#  #' My median
+#  #'
+#  #' @param x Vector of Numeric values
+#  #' @inheritParams stats::median
+#  #'
+#  #' @return
+#  #' Median of vector x
+#  #' @export
+#  #'
+#  #' @examples
+#  my_median <- function(x, na.rm = TRUE) {
+#    if (!is.numeric(x)) {stop("x should be numeric")}
+#    stats::median(x, na.rm = na.rm)
+#  }
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  my_median(1:12)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  test_that("my_median works properly and show error if needed", {
+#    expect_true(my_median(1:12) == 6.5)
+#    expect_error(my_median("text"))
+#  })
+
