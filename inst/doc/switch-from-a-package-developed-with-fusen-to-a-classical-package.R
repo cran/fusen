c434f4cffd6b6964a891f8fba652262255dc5e6a
Vincent Guyader <vincent@thinkr.fr>
2025-08-29 16:00:08 +0000

    version 0.7.2

--- inst/doc/switch-from-a-package-developed-with-fusen-to-a-classical-package.R
+++ inst/doc/switch-from-a-package-developed-with-fusen-to-a-classical-package.R
@@ -8,108 +8,108 @@ knitr::opts_chunk$set(
 library(fusen)
 
 ## ----examples-sepuku----------------------------------------------------------
-# #' \dontrun{
-# sepuku()
-# # If you want to force the cleaning, you can use the force argument
-# sepuku(force = TRUE)
-# 
-# # Example with a dummy package
-# dummypackage <- tempfile("sepuku.example")
-# dir.create(dummypackage)
-# fill_description(pkg = dummypackage, fields = list(Title = "Dummy Package"))
-# 
-# usethis::with_project(dummypackage, {
-#   # Add licence
-#   usethis::use_mit_license("John Doe")
-# 
-#   dir.create(file.path(dummypackage, "dev"))
-#   dir.create(file.path(dummypackage, "dev", "flat_history"))
-# 
-#   # We add 2 flat files in the package and inflate them
-#   dev_file1 <- add_minimal_flat(
-#     pkg = dummypackage,
-#     flat_name = "flat1.Rmd",
-#     open = FALSE
-#   )
-# 
-#   dev_file2 <- add_minimal_flat(
-#     pkg = dummypackage,
-#     flat_name = "flat2.Rmd",
-#     open = FALSE
-#   )
-# 
-#   inflate(
-#     pkg = dummypackage,
-#     flat_file = dev_file1,
-#     vignette_name = "Get started",
-#     check = FALSE,
-#     open_vignette = FALSE,
-#     document = TRUE,
-#     overwrite = "yes"
-#   )
-# 
-#   inflate(
-#     pkg = dummypackage,
-#     flat_file = dev_file2,
-#     vignette_name = "Get started 2",
-#     check = FALSE,
-#     open_vignette = FALSE,
-#     document = TRUE,
-#     overwrite = "yes"
-#   )
-# 
-#   # We deprecate the first flat file, which will be moved to the flat_history folder
-#   deprecate_flat_file(
-#     file.path(dummypackage, "dev", "flat_flat1.Rmd")
-#   )
-# 
-#   # We create 2 flat files with the qmd extension
-#   file.create(file.path(dummypackage, "dev", "flat_history", "flat_old.qmd"))
-#   file.create(file.path(dummypackage, "dev", "flat_qmd.qmd"))
-# 
-#   sepuku(force = TRUE)
-# 
-#   # We check that the fusen configuration file has been deleted
-#   file.exists(
-#     file.path(dummypackage, "dev", "config_fusen.yaml")
-#   )
-# 
-#   # We check that all the flat files have been deleted
-#   length(
-#     list.files(
-#       file.path(dummypackage, "dev"),
-#       pattern = "^flat.*\\.Rmd"
-#     )
-#   )
-# 
-#   length(
-#     list.files(
-#       file.path(dummypackage, "dev"),
-#       pattern = "^flat.*\\.qmd"
-#     )
-#   )
-# 
-# 
-#   length(
-#     list.files(
-#       file.path(dummypackage, "dev", "flat_history"),
-#       pattern = "^flat.*\\.Rmd"
-#     )
-#   )
-# 
-# 
-#   length(
-#     list.files(
-#       file.path(dummypackage, "dev", "flat_history"),
-#       pattern = "^flat.*\\.qmd"
-#     )
-#   )
-# 
-#   # We check that all the files with fusen tags have been cleaned
-#   length(fusen:::find_files_with_fusen_tags(pkg = dummypackage))
-# })
-# 
-# # Clean the temporary directory
-# unlink(dummypackage, recursive = TRUE)
-# #' }
+#  #' \dontrun{
+#  sepuku()
+#  # If you want to force the cleaning, you can use the force argument
+#  sepuku(force = TRUE)
+#  
+#  # Example with a dummy package
+#  dummypackage <- tempfile("sepuku.example")
+#  dir.create(dummypackage)
+#  fill_description(pkg = dummypackage, fields = list(Title = "Dummy Package"))
+#  
+#  usethis::with_project(dummypackage, {
+#    # Add licence
+#    usethis::use_mit_license("John Doe")
+#  
+#    dir.create(file.path(dummypackage, "dev"))
+#    dir.create(file.path(dummypackage, "dev", "flat_history"))
+#  
+#    # We add 2 flat files in the package and inflate them
+#    dev_file1 <- add_minimal_flat(
+#      pkg = dummypackage,
+#      flat_name = "flat1.Rmd",
+#      open = FALSE
+#    )
+#  
+#    dev_file2 <- add_minimal_flat(
+#      pkg = dummypackage,
+#      flat_name = "flat2.Rmd",
+#      open = FALSE
+#    )
+#  
+#    inflate(
+#      pkg = dummypackage,
+#      flat_file = dev_file1,
+#      vignette_name = "Get started",
+#      check = FALSE,
+#      open_vignette = FALSE,
+#      document = TRUE,
+#      overwrite = "yes"
+#    )
+#  
+#    inflate(
+#      pkg = dummypackage,
+#      flat_file = dev_file2,
+#      vignette_name = "Get started 2",
+#      check = FALSE,
+#      open_vignette = FALSE,
+#      document = TRUE,
+#      overwrite = "yes"
+#    )
+#  
+#    # We deprecate the first flat file, which will be moved to the flat_history folder
+#    deprecate_flat_file(
+#      file.path(dummypackage, "dev", "flat_flat1.Rmd")
+#    )
+#  
+#    # We create 2 flat files with the qmd extension
+#    file.create(file.path(dummypackage, "dev", "flat_history", "flat_old.qmd"))
+#    file.create(file.path(dummypackage, "dev", "flat_qmd.qmd"))
+#  
+#    sepuku(force = TRUE)
+#  
+#    # We check that the fusen configuration file has been deleted
+#    file.exists(
+#      file.path(dummypackage, "dev", "config_fusen.yaml")
+#    )
+#  
+#    # We check that all the flat files have been deleted
+#    length(
+#      list.files(
+#        file.path(dummypackage, "dev"),
+#        pattern = "^flat.*\\.Rmd"
+#      )
+#    )
+#  
+#    length(
+#      list.files(
+#        file.path(dummypackage, "dev"),
+#        pattern = "^flat.*\\.qmd"
+#      )
+#    )
+#  
+#  
+#    length(
+#      list.files(
+#        file.path(dummypackage, "dev", "flat_history"),
+#        pattern = "^flat.*\\.Rmd"
+#      )
+#    )
+#  
+#  
+#    length(
+#      list.files(
+#        file.path(dummypackage, "dev", "flat_history"),
+#        pattern = "^flat.*\\.qmd"
+#      )
+#    )
+#  
+#    # We check that all the files with fusen tags have been cleaned
+#    length(fusen:::find_files_with_fusen_tags(pkg = dummypackage))
+#  })
+#  
+#  # Clean the temporary directory
+#  unlink(dummypackage, recursive = TRUE)
+#  #' }
 
--- inst/doc/tips-and-tricks.R
+++ inst/doc/tips-and-tricks.R
@@ -5,21 +5,21 @@ knitr::opts_chunk$set(
 )
 
 ## ----eval=FALSE---------------------------------------------------------------
-# inflate(vignette_name = c("Super title" = "01-Super Vignette Index Entry"))
+#  inflate(vignette_name = c("Super title" = "01-Super Vignette Index Entry"))
 
 ## ----eval=FALSE---------------------------------------------------------------
-# inflate(vignette_name = NA)
+#  inflate(vignette_name = NA)
 
 ## ----eval=FALSE---------------------------------------------------------------
-# # The path relative to the "tests/testthat" directory for tests
-# the_file <- "my_file.csv"
-# if (!file.exists(the_file)) {
-#   # The path to use during dev in the flat file
-#   the_file <- file.path("tests", "testthat", the_file)
-#   if (!file.exists(the_file)) {
-#     stop(the_file, " does not exist")
-#   }
-# }
-# 
-# my_file <- read.csv(the_file)
+#  # The path relative to the "tests/testthat" directory for tests
+#  the_file <- "my_file.csv"
+#  if (!file.exists(the_file)) {
+#    # The path to use during dev in the flat file
+#    the_file <- file.path("tests", "testthat", the_file)
+#    if (!file.exists(the_file)) {
+#      stop(the_file, " does not exist")
+#    }
+#  }
+#  
+#  my_file <- read.csv(the_file)
 
